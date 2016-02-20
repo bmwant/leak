068405813c01d93d96fa098335cacc5d885cf88f
Misha Behersky <bmwant@gmail.com>
2016-02-20 12:57:54 +0200

    Add file version_parser.py

---
+++ version_parser.py
@@ -0,0 +1,14 @@
+def versions_split(version_str, type_applyer=int):
+    dots_count = version_str.count('.')
+    if dots_count == 0:
+        major, minor, patch = version_str, 0, 0
+    elif dots_count == 1:
+        major, minor = version_str.split('.')
+        patch = 0
+    elif dots_count == 2:
+        major, minor, patch = version_str.split('.')
+    else:
+        logger.debug('Incorrect version "%s". Move to bottom when sorting' % version_str)
+        major, minor, patch = 0, 0, 0
+
+    return map(type_applyer, (major, minor, patch))
