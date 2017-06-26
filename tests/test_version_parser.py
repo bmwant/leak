a52c84f092d89f89130c2696c98779e955f083dc
Misha Behersky <bmwant@gmail.com>
2017-06-26 12:17:09 +0300

    Add tests for version splitting

--- tests/test_version_parser.py
+++ tests/test_version_parser.py
@@ -4,7 +4,15 @@ from leak.version_parser import versions_split
 
 
 def test_versions_split():
-    pass
+    assert versions_split('1.8.1') == [1, 8, 1]
+    assert versions_split('1.4') == [1, 4, 0]
+    assert versions_split('2') == [2, 0, 0]
+
+
+def test_versions_split_str_mapping():
+    assert versions_split('1.11rc1', type_applyer=str) == ['1', '11rc1', '0']
+    assert versions_split('1.10b1', type_applyer=str) == ['1', '10b1', '0']
+    assert versions_split('text', type_applyer=str) == ['text', '0', '0']
 
 
 def test_wrong_versions_split():
